4df3c467ff6d7e51dfe6fbeedd680a698c8c49f3
achimmertens <achim_mertens@gmx.de>
2025-07-04 08:44:27 +0200

    chore: Doku verbessert

--- keycloak/start_keycloak_server.sh
+++ keycloak/start_keycloak_server.sh
@@ -1,12 +1,13 @@
 # Start Keycloak server with network configuration
-podman run -p 8081:8080 \
-  --network midpoint_net \
-  -e KEYCLOAK_ADMIN=admin \
-  -e KEYCLOAK_ADMIN_PASSWORD=admin \
-  -e KC_PROXY=edge \
-  -e KC_HOSTNAME_STRICT=false \
-  -e KC_HOSTNAME_STRICT_HTTPS=false \
-  -e KC_HOSTNAME=localhost:8081 \
-  --name keycloak_server \
-  quay.io/keycloak/keycloak:21.0.2 \
-  start-dev
+# podman run -p 8081:8080 \
+#   --network midpoint_net \
+#   -e KEYCLOAK_ADMIN=admin \
+#   -e KEYCLOAK_ADMIN_PASSWORD=admin \
+#   -e KC_PROXY=edge \
+#   -e KC_HOSTNAME_STRICT=false \
+#   -e KC_HOSTNAME_STRICT_HTTPS=false \
+#   -e KC_HOSTNAME=localhost:8081 \
+#   --name keycloak_server \
+#   quay.io/keycloak/keycloak:21.0.2 \
+#   start-dev
+podman start keycloak_server
