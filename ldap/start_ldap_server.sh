34ad5c216b8af992d21d67e79aafef19981966d5
achimmertens <achim_mertens@gmx.de>
2025-08-07 08:17:09 +0200

    chore: Alle Podman Container starten nun mit festen, vorgegebenen IP Adressen.

--- ldap/start_ldap_server.sh
+++ ldap/start_ldap_server.sh
@@ -1,11 +1,13 @@
 # Start the LDAP server with network configuration
+podman rm ldap_server
 
-# podman run --network midpoint_net \
-#  -p 3389:3389 -p 3636:3636 \
-#  --name ldap_server \
-#  -e DS_DM_PASSWORD=1234 \
-#  -e DS_SUFFIX_NAME="dc=example,dc=com" \
-#  -v ldap_data:/data \
-#  389ds/dirsrv
+podman run --network midpoint_net \
+ -p 3389:3389 -p 3636:3636 \
+ --name ldap_server \
+ --ip 10.89.0.10 \
+ -e DS_DM_PASSWORD=1234 \
+ -e DS_SUFFIX_NAME="dc=example,dc=com" \
+ -v ldap_data:/data \
+ 389ds/dirsrv
 
-podman start -a ldap_server
+# podman start -a ldap_server
--- nginx/start_nginx.sh
+++ nginx/start_nginx.sh
@@ -16,16 +16,16 @@ touch /d/IAM/nginx/logs/access.log
 
 # Start the container
 podman run --name nginx_webserver \
+  --ip 10.89.0.21 \
   --network midpoint_net \
   --dns 127.0.0.11 \
   --mount type=bind,source=/d/IAM/nginx/nginx.conf,target=/etc/nginx/nginx.conf,readonly \
   --mount type=bind,source=/d/IAM/nginx/sites-enabled,target=/etc/nginx/sites-enabled,readonly \
   --mount type=bind,source=/d/IAM/nginx/html,target=/usr/share/nginx/html \
   --mount type=bind,source=/d/IAM/nginx/logs,target=/var/log/nginx \
-  -p 8080:80 \
-  nginx:latest
+  -p 8080:80 nginx:latest
 
 # Install diagnostic tools
 # podman exec -it nginx_webserver bin/bash
 # apt-get update && apt-get install -y curl
-
+# podman start nginx_webserver -a
